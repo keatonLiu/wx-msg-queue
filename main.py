1b524173ae617b1e971b0de1eaf4acd21f8065d3
keatonLiu <lxtxiaotong@foxmail.com>
2025-09-29 23:23:52 +0800

    async wx_send

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ LOGGING_CONFIG["formatters"]["access"][
     "fmt"] = '%(asctime)s [%(levelname)s] %(client_addr)s - "%(request_line)s" %(status_code)s'
 
 main_queue = Queue()
-msg_queue_map = defaultdict(lambda: Queue(maxsize=1))
 pusher = WxPusher()
 
 
@@ -40,30 +39,26 @@ app = FastAPI(lifespan=lifespan)
 @app.post("/msg")
 async def forward_msg(request: Request):
     data = await request.json()
-    msg_id = uuid.uuid4().hex
-    # 将消息和请求者URL加入队列
-    await main_queue.put((msg_id, data))
-    # 阻塞等待响应返回
-    response = await msg_queue_map[msg_id].get()
-    del msg_queue_map[msg_id]
-    return response
+    await main_queue.put(data)
+    return {"code": 0, "msg": "ok", "data": {
+        "size": main_queue.qsize()
+    }}
 
 
 async def msg_consume():
     logger.info("Start msg consume")
     start = 0
     while True:
-        msg_id, message = await main_queue.get()
+        message = await main_queue.get()
         if (remain := SEND_DELAY - (time.time() - start)) > 0:
             await asyncio.sleep(remain)
         start = time.time()
         try:
             # 向后端服务器发送请求
             response = await pusher.send(message)
-        except Exception as e:
-            response = {"code": -1, "error": str(e)}
+            logger.info(f"Send msg consume response: {response}")
+        except Exception as e:  # noqa
             logger.error(traceback.format_exc())
-        await msg_queue_map[msg_id].put(response)
 
 
 if __name__ == '__main__':
